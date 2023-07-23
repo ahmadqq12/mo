0457f4d8185eb693d67cb97eb9087ab723f6006f
ahmadqq12 <140335313+ahmadqq12@users.noreply.github.com>
2023-07-24 02:05:36 +0300

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = 'https://movie-bot-swart.vercel.app'
+URL = 'https://mo-ahmadqq12.vercel.app'
 bot = Bot(TOKEN)
 
 
